021a07eed47225be1938a02baf6824fada75eda8
zhangyf <zhangyf@zhangyifandeMacBook-Air.local>
2014-01-29 13:45:46 +0800

    Evaluate_Reverse_Polish_Notation

---
+++ Evaluate_Reverse_Polish_Notation.cc
@@ -0,0 +1,58 @@
+#include <vector>
+#include <iostream>
+#include <stack>
+#include <sstream>
+
+using namespace std;
+
+class Solution {
+public:
+	int evalRPN(vector<string> &tokens) {
+		stack<int> formula;
+		for (vector<string>::iterator it = tokens.begin(); it != tokens.end(); ++it) {
+			if (*it == "+") {
+				int a = formula.top();
+				formula.pop();
+				int b = formula.top();
+				formula.pop();
+				formula.push(b + a);
+			} else if (*it == "-") {
+				int a = formula.top();
+				formula.pop();
+				int b = formula.top();
+				formula.pop();
+				formula.push(b - a);
+			}
+			else if (*it == "*") {
+				int a = formula.top();
+				formula.pop();
+				int b = formula.top();
+				formula.pop();
+				formula.push(b * a);
+			} else if (*it == "/") {
+				int a = formula.top();
+				formula.pop();
+				int b = formula.top();
+				formula.pop();
+				formula.push(b / a);
+			} else {
+				stringstream ss;
+				ss << *it;
+				int i;
+				ss >> i;
+				formula.push(i);
+			}
+		}
+		int ret = formula.top();
+		formula.pop();
+		return ret;
+	}
+};
+
+int main() {
+	Solution sol = Solution();
+	string input[] = {"4", "13", "5", "/", "+"};
+	vector<string> tokens(input, input + sizeof(input) / sizeof(string));
+	cout << sol.evalRPN(tokens) << endl;
+	return 0;
+}
