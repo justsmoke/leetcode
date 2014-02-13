07a2eaa2c9de014580ad7b9ae3d599aea70a3b5a
justsmoke <just.ff.smoke@gmail.com>
2014-02-14 02:17:22 +0800

    Best_Time_to_Buy_and_Sell_Stock_II.cc

---
+++ Best_Time_to_Buy_and_Sell_Stock_II.cc
@@ -0,0 +1,49 @@
+//Say you have an array for which the ith element is the price of a given stock on day i.
+//If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+	int maxProfit(vector<int> &prices) {
+		int sz = prices.size();
+		if (sz < 2) {
+			return 0;
+		}
+		int i = 0, sum = 0;
+		int low, high;
+		while (i < sz - 1) {
+			while (i < sz - 1 && prices[i] >= prices[i+1]) {
+				++ i;
+			}
+			if (i == sz - 1) {
+				break;
+			}
+			low = prices[i++];
+			while (i < sz - 1 && prices[i] <= prices[i+1]) {
+				++ i;
+			}
+			high = prices[i++];
+			sum += high - low;
+		}
+		return sum;
+	}
+};
+
+int main() {
+	Solution sol = Solution();
+	vector<int> p;
+	p.push_back(5);
+	p.push_back(3);
+	p.push_back(4);
+	p.push_back(9);
+	p.push_back(1);
+	p.push_back(4);
+	p.push_back(3);
+	p.push_back(2);
+	cout << sol.maxProfit(p) << endl;
+	return 0;
+}
