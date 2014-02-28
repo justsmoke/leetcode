0b53f438009e8c65b51fbb70e6811649c7b858ab
justsmoke <just.ff.smoke@gmail.com>
2014-02-28 22:59:08 +0800

    Best_Time_to_Buy_and_Sell_Stock_II.cc

--- Best_Time_to_Buy_and_Sell_Stock_II.cc
+++ Best_Time_to_Buy_and_Sell_Stock_II.cc
@@ -1,5 +1,5 @@
 //Say you have an array for which the ith element is the price of a given stock on day i.
-//If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+//Design an algorithm to find the maximum profit. You may complete as many transactions as you like (ie, buy one and sell one share of the stock multiple times). However, you may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
 
 #include <vector>
 #include <iostream>
