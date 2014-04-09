cb1b4e0da17933549bdb1b88ecd924f6f55c699b
justsmoke <just.ff.smoke@gmail.com>
2014-04-09 13:39:00 +0800

    non-recursive dfs

--- Word_Search.cc
+++ Word_Search.cc
@@ -12,102 +12,55 @@ using namespace std;
 class Solution {
 public:
 	bool exist(vector<vector<char> > &board, string word) {
-		if (word.length() == 0) {
+		if (word == "") {
 			return true;
 		}
-		int m = board.size();
-		if (m == 0) {
+		int row = board.size();
+		if (row == 0 || board[0].size() == 0) {
 			return false;
 		}
-		int n = board[0].size();
-		if (n == 0) {
-			return false;
-		}
-		//Up : 0, Down : 1, Left : 2, Right : 3
-		vector<vector<vector<int>>> way(m * n, vector<vector<int>>(word.length(), vector<int>(4, 0)));
-		unordered_map<string, vector<int>> step;
-		vector<bool> visited(m * n, false);
-		stack<int> path;
-		string s("00");
-		for (int i = 0; i != word.length() - 1; ++ i) {
-			step[word.substr(i, 2)].push_back(i);
-		}
-		for (int k = 0; k != m * n; ++ k) {
-			int i = k / n;
-			int j = k % n;
-			s[0] = board[i][j];
-			vector<pair<string, int>> edge;
-			if (i != 0) {
-				s[1] = board[i - 1][j];
-				edge.push_back(make_pair(s, 0));
+		int col = board[0].size();
+		int start = 0, x, y, pos, last;
+		while (1) {
+			while (start != row * col && board[start / col][start % col] != word[0]) {
+				++ start;
 			}
-			if (i != m - 1) {
-				s[1] = board[i + 1][j];
-				edge.push_back(make_pair(s, 1));
-			}
-			if (j != 0) {
-				s[1] = board[i][j - 1];
-				edge.push_back(make_pair(s, 2));
-			}
-			if (j != n - 1) {
-				s[1] = board[i][j + 1];
-				edge.push_back(make_pair(s, 3));
-			}
-			for (auto u = edge.begin(); u != edge.end(); ++ u) {
-				for (auto it = step[u->first].begin(); it != step[u->first].end(); ++ it) {
-					way[k][*it][u->second] = 1;
-				}
-			}
-		}
-		int start = 0, pos = 0, last;
-		while (start != m * n) {
-			for (; start != m * n; ++ start) {
-				if (board[start / n][start % n] == word[0]) {
-					break;
-				}
-			}
-			if (start == m * n) {
+			if (start == row * col) {
 				break;
 			}
-			visited[start] = true;
-			path.push(start);
-			pos = 1;
-			while (!path.empty() && pos != word.length()) {
-				last = path.top();
-				if (way[last][pos - 1][0] && !visited[last - n]) {
-					way[last][pos - 1][0] = 0;
-					++ pos;
-					visited[last - n] = true;
-					path.push(last - n);
-					continue;
+			stack<pair<int, int>> dfs;
+			dfs.push(make_pair(start, 0));
+			vector<int> path;
+			vector<bool> visited(row * col, false);
+			while (!dfs.empty()) {
+				pair<int, int> tp = dfs.top();
+				last = tp.first;
+				pos = tp.second;
+				x = last / col;
+				y = last % col;
+				dfs.pop();
+				while (path.size() > pos) {
+					visited[path.back()] = false;
+					path.pop_back();
 				}
-				if (way[last][pos - 1][1] && !visited[last + n]) {
-					way[last][pos - 1][1] = 0;
-					++ pos;
-					visited[last + n] = true;
-					path.push(last + n);
-					continue;
+				path.push_back(last);
+				visited[last] = true;
+				++ pos;
+				if (pos == word.length()) {
+					return true;
 				}
-				if (way[last][pos - 1][2] && !visited[last - 1]) {
-					way[last][pos - 1][2] = 0;
-					++ pos;
-					visited[last - 1] = true;
-					path.push(last - 1);
-					continue;
+				if (x != 0 && board[x - 1][y] == word[pos] && !visited[last - col]) {
+					dfs.push(make_pair(last - col, pos));
 				}
-				if (way[last][pos - 1][3] && !visited[last + 1]) {
-					way[last][pos - 1][3] = 0;
-					++ pos;
-					visited[last + 1] = true;
-					path.push(last + 1);
-					continue;
+				if (x != row - 1 && board[x + 1][y] == word[pos] && !visited[last + col]) {
+					dfs.push(make_pair(last + col, pos));
 				}
-				path.pop();
-				-- pos;
-				visited[last] = false;
-			}
-			if (pos == word.length()) {
-				return true;
+				if (y != 0 && board[x][y - 1] == word[pos] && !visited[last - 1]) {
+					dfs.push(make_pair(last - 1, pos));
+				}
+				if (y != col - 1 && board[x][y + 1] == word[pos] && !visited[last + 1]) {
+					dfs.push(make_pair(last + 1, pos));
+				}
 			}
 			++ start;
 		}
@@ -118,25 +71,16 @@ public:
 int main() {
 	Solution sol = Solution();
 	vector<vector<char>> board;
+	string s = "ABCESFESADEE";
+	int i = 0;
 	vector<char> v;
-	v.push_back('A');
-	v.push_back('B');
-	v.push_back('C');
-	v.push_back('E');
-	board.push_back(v);
-	v.clear();
-	v.push_back('S');
-	v.push_back('F');
-	v.push_back('C');
-	v.push_back('S');
-	board.push_back(v);
-	v.clear();
-	v.push_back('A');
-	v.push_back('D');
-	v.push_back('E');
-	v.push_back('E');
-	board.push_back(v);
-	string s;
+	for (int row = 0; row != 3; ++ row) {
+		v.clear();
+		for (int col = 0; col != 4; ++ col) {
+			v.push_back(s[i++]);
+		}
+		board.push_back(v);
+	}
 	cin >> s;
 	if (sol.exist(board, s)) {
 		cout << "YES!" << endl;
